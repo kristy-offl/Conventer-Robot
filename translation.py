721ae9df820c8c77ecfe33579bb8c41b4f54083c
kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-04-13 22:23:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,7 +5,9 @@ class Translation(object):
 
       START = """Hello {0}
 
-I am a converter clone of [Convert Ns Bot](https://telegram.dog/convert_Ns_bot) by {1}
+🏷️ I am TG Files to Video or Videos to File Converter Bot
+
+For more details ser /help .
 
 I can convert file to video or video to file with custom thumbnail support.
 """
@@ -23,7 +25,7 @@ I can convert file to video or video to file with custom thumbnail support.
 
 4. Reply to file /converttovideo for converting into video.
 
-**SUPPORT GROUP:** [NS Bot Supporters](https://telegram.dog/Ns_Bot_supporters)
+**SUPPORT GROUP:** [ML Support Group](https://telegram.dog/ML_SupportGroup)
 """
 
 
@@ -34,11 +36,11 @@ I can convert file to video or video to file with custom thumbnail support.
 
 **🧰 Framework:** Pyrogram
 
-**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)
+**👨‍💻 Developer:** [Itz_Me_Malayaali](https://t.me/Kristy_Offl)
 
-**📮 Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+**📮 Channel:** [NS BOT UPDATES](https://t.me/ML_BotUpdates)
 
-**👥 Group:** [NS BOT SUPPOTERS](https://t.me/Ns_Bot_supporters)
+**👥 Group:** [NS BOT SUPPOTERS](https://t.me/ML_SupportGroup)
 
 **💻 Source Code:**[Press Me](https://github.com/Ns-AnoNymouS/TG-CONVERT-BOT)
 
